9d719c18e53d3994a75497d683027ef1363fa7f0
krguang <526110805@qq.com>
2018-07-09 17:42:06 +0800

    为了提高测量电流的范围，把采样电阻由51R改为39R，更改了对应的采样公式。

--- Src/dataProcessing.c
+++ Src/dataProcessing.c
@@ -19,7 +19,7 @@ uint16_t humiCurrent;
 uint16_t humiOpening;
 uint16_t humiOpeningFromPLC;
 uint16_t humiCurrentUpperLimit;
-uint64_t humiVoltage;
+uint16_t humiVoltage = 380;
 uint16_t powerProportion;
 
 uint16_t drainWaterTime;
@@ -31,6 +31,7 @@ volatile uint16_t ADC_ConvertedValue[3];
 uint32_t ADC_Average[3];
 
 uint8_t humiMode;
+
 
 static void adcProcesdsing() {
 
@@ -44,9 +45,12 @@ static void adcProcesdsing() {
 		}
 	}
 
-	humiOpening = ADC_Average[0] *10/ 4095;
-	humiCurrent = ADC_Average[1] * 8 / 4095;//0-80A对应0-3.3V
-	powerProportion = ADC_Average[2] * (1001 - 250) / 4095 / 100 + 250;
+	humiOpening = ADC_Average[0] *10/ 4096;
+	//humiCurrent = ADC_Average[1] * 8 / 4095;//0-80A对应0-3.3V
+	//humiCurrent = ADC_Average[1] * 6 / 5316;//5316=(3.06*1.4/3.3)*4095
+	humiCurrent = ADC_Average[1] *6/4096;//选用39R电阻。0-60A-->0-60ma-->0-0.06*39*1.414V-->0-3.3V-->0-4095
+										//0-600 对应 0-4095.
+	powerProportion = ADC_Average[2] * (1001 - 250) / 4096 / 100 + 250;
 
 	for (uint8_t i = 0; i < 3; i++)
 	{
@@ -55,6 +59,27 @@ static void adcProcesdsing() {
 	//printf("humiCurrent = %d\n", humiCurrent);
 }
 
+/*************************************由功率计算出额定电流************************************/
+static uint16_t getIFromP(uint16_t p) {
+
+	if (380 == humiVoltage || 400 == humiVoltage)
+	{
+		if (p < 17500)
+		{
+			return p*1.732 / 3 / humiVoltage * 10;
+		}
+		else
+		{
+			return p*1.732 / 3 / humiVoltage * 10 / 2;		//当功率大于等于17500时，供电线由3根变为6根，单根电流减半
+		}
+	}
+
+	if (220 == humiVoltage)
+	{
+		return p / humiVoltage * 10;
+	}
+
+}
 
 void dialSwitchInit() {
 
@@ -76,70 +101,73 @@ void dialSwitchInit() {
 
 
 	/***********************************  加湿电压选择  *************************************/
-	if ((readS1Pin1 == 1) && (readS1Pin2 == 1))
+	if ((readS1Pin3 == 1) && (readS1Pin4 == 1))
 	{
 		humiVoltage = 380;
 	}
 
-	if ((readS1Pin1 == 0) && (readS1Pin2 == 0))
+	if ((readS1Pin3 == 0) && (readS1Pin4 == 0))
 	{
 		humiVoltage = 220;
 	}
 
-	if ((readS1Pin1 == 0) && (readS1Pin2 == 1))
+	if ((readS1Pin3 == 0) && (readS1Pin4 == 1))
 	{
-		humiVoltage = 440;
+		humiVoltage = 400;
 	}
 
 	/***********************************  加湿电流选择  *************************************/
+	/*
+	*	humiCurrentUpperLimit为实际额定电流*10，方便后面数据处理
+	*/
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 1) && (readS2Pin4 == 0) && (readS2Pin5 == 0))
 	{
-		humiCurrentUpperLimit = 2 * 11;
+		humiCurrentUpperLimit = getIFromP(1500);
 	}
 
 	if ((readS2Pin2 == 0) && (readS2Pin3 == 1) && (readS2Pin4 == 0) && (readS2Pin5 == 0))
 	{
-		humiCurrentUpperLimit = 3 * 11;
+		humiCurrentUpperLimit = getIFromP(2300);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 0) && (readS2Pin4 == 0) && (readS2Pin5 == 0))
 	{
-		humiCurrentUpperLimit = 4 * 11;
+		humiCurrentUpperLimit = getIFromP(3000);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 0) && (readS2Pin4 == 1) && (readS2Pin5 == 0))
 	{
-		humiCurrentUpperLimit = 8 * 11;
+		humiCurrentUpperLimit = getIFromP(6100);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 1) && (readS2Pin4 == 1) && (readS2Pin5 == 0))
 	{
-		humiCurrentUpperLimit = 13 * 11;
+		humiCurrentUpperLimit = getIFromP(9800);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 0) && (readS2Pin4 == 0) && (readS2Pin5 == 1))
 	{
-		humiCurrentUpperLimit = 15 * 11;
+		humiCurrentUpperLimit = getIFromP(11400);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 1) && (readS2Pin4 == 0) && (readS2Pin5 == 1))
 	{
-		humiCurrentUpperLimit = 23 * 11;
+		humiCurrentUpperLimit = getIFromP(17500);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 0) && (readS2Pin4 == 1) && (readS2Pin5 == 1))
 	{
-		humiCurrentUpperLimit = 32 * 11;
+		humiCurrentUpperLimit = getIFromP(24300);
 	}
 
 	if ((readS2Pin2 == 1) && (readS2Pin3 == 1) && (readS2Pin4 == 1) && (readS2Pin5 == 1))
 	{
-		humiCurrentUpperLimit = 45 * 11;
+		humiCurrentUpperLimit = getIFromP(34200);
 	}
 
 	if ((readS2Pin2 == 0) && (readS2Pin3 == 1) && (readS2Pin4 == 1) && (readS2Pin5 == 1))
 	{
-		humiCurrentUpperLimit = 65 * 11;
+		humiCurrentUpperLimit = getIFromP(49400);
 	}
 	
 /******************************************  额外排水时间  ****************************************/
@@ -162,7 +190,6 @@ void dialSwitchInit() {
 	{
 		drainWaterTime = 0;
 	}
-	
 }
 
 
@@ -176,6 +203,4 @@ void dataProcessing() {
 
 	ctrlToPLCTemp[0] = humiCurrent;
 	humiOpeningFromPLC = ctrlToPLCTemp[5];
-
-
 }
